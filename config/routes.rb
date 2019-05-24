3024b0ef57066d16761e17383d50e3edf9eab8da
japalominos <japalominos@miuandes.cl>
2019-05-23 23:49:49 -0400

    lab 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, :tickets, :user_tickets, :events
 
   root :to => 'sessions#new'
-
+  
   post '/login',   to: 'sessions#create', as: :log_in
   delete '/log_out' => 'sessions#destroy', as: :log_out
 
   get '/sign_in' => 'registrations#new', as: :registrations
   post '/sign_in' => 'registrations#create', as: :sign_in
-
+  
 end
